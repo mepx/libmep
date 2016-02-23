b664b54c49df21078875e0c8bdb725bbbfd730fc
mihaioltean <moltean@cs.ubbcluj.ro>
2016-02-23 14:28:27 +0200

    updated

--- tests/basic_libmep_test.cpp
+++ tests/basic_libmep_test.cpp
@@ -12,6 +12,9 @@ int main(void)
 {
 	t_mep mep;
 	t_mep_data training_data;
+	t_mep_operators mep_operators;
+	t_mep_parameters mep_parameters;
+	t_mep_constants mep_constants;
 
 	if (!training_data.from_csv("building1.csv")) {
 		printf("Cannot load training data! Please make sure that the path to file is correct!");
@@ -21,20 +24,25 @@ int main(void)
 	}
 
 	mep.set_training_data(&training_data);
-	mep.set_addition(true);
-	mep.set_subtraction(true);
-	mep.set_multiplication(true);
+	mep_operators.set_addition(true);
+	mep_operators.set_subtraction(true);
+	mep_operators.set_multiplication(true);
 
-	mep.set_num_subpopulations(1);
-	mep.set_num_generations(50);
-	mep.set_code_length(20);
-	mep.set_problem_type(0); // regression
+	mep.set_operators(&mep_operators);
+
+	mep_parameters.set_num_subpopulations(1);
+	mep_parameters.set_num_generations(50);
+	mep_parameters.set_code_length(20);
+	mep_parameters.set_problem_type(0); // regression
+
+	mep.set_parameters(&mep_parameters);
+	mep.set_constants(&mep_constants);
 
 	printf("Evolving ... \n");
 	generation_index = 0;
 	mep.start(on_generation, NULL, NULL);
 
-	double error = mep.get_best_training_error(0, mep.get_num_generations() - 1);
+	double error = mep.get_best_training_error(0, mep_parameters.get_num_generations() - 1);
 	
 	printf("\nError = %lf\n", error);
 
