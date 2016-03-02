ddb761067e65f061fb19e150f77cd26616e7c4c2
mihaioltean <moltean@cs.ubbcluj.ro>
2016-03-02 07:35:28 +0200

    defined some error types

--- include/mep_utils.h
+++ include/mep_utils.h
@@ -13,4 +13,8 @@ int sort_function_value_class(const void *a, const void *b);
 
 int my_strcmp(char *s1, char *s2);
 
+#define MEP_OK 0
+#define E_NO_DATA 1
+#define E_DATA_MUST_HAVE_REAL_TYPE 1
+
 #endif
